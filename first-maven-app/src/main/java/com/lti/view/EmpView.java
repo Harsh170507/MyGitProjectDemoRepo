b75a23bdad1c6ed1ae3033897bfe5a460a94ae20
Harsh170507 <hbhatia19981207@gmail.com>
2021-03-25 12:42:59 +0530

    Second Commit

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -6,8 +6,8 @@ public class EmpView {
 
 	public static void main(String[] args) {
 		Employee emp = new Employee();
-		emp.setEmpId(1001);
-		emp.setEmpName("Jack Ryan");
+		emp.setEmpId(1002);
+		emp.setEmpName("Jeshann Bhatia");
 		System.out.println("Employee Id is : " + emp.getEmpId() + "Employee Name is : " + emp.getEmpName());
 	}
 
